2daafb812f4d1bb8a0ddf0d20d1cbad4cd1173c0
anikat2 <anikathakur212@gmail.com>
2024-06-09 13:13:51 -0400

    websocket

--- server.js
+++ server.js
@@ -1,40 +1,17 @@
-const express = require('express');
-const dotEnv = require('dotenv');
-const http = require('http');
 const WebSocket = require('ws');
 
-dotEnv.config();
-const app = express();
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const wss = new WebSocket.Server({ port: 8080 });
 
-app.get('/', function (req, res) {
-  res.send('Server is runninggg...');
-});
-
-// WebSocket connection handler
 wss.on('connection', (ws) => {
-  console.log('New client connected');
-
-  // WebSocket message handler
-  ws.on('message', (message) => {
-    console.log('Received message:', message);
-
-    // Broadcast the received message to all connected clients except the sender
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
+    ws.on('message', (message) => {
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
     });
-  });
 
-  // WebSocket close handler
-  ws.on('close', () => {
-    console.log('Client disconnected');
-  });
+    ws.send('Welcome to the WebSocket server');
 });
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log('Server running at port ' + port);
-});
+console.log('WebSocket server is running on ws://localhost:8080');
